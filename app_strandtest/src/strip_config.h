5ada618173d22cd1a0b195d1ff5b0b3a7e750dc3
teachop <teachop@hotmail.com>
2014-04-20 07:52:02 -0400

    adjustable pulse timing via strip_header

--- app_strandtest/src/strip_config.h
+++ app_strandtest/src/strip_config.h
@@ -9,4 +9,9 @@
 // neopixel strip length
 #define LEDS 60
 
+// timing adjustment, 10 nanosecond increments
+//#define NEO_P1 42
+//#define NEO_P2 38
+//#define NEO_P3 46
+
 #endif // __STRIP_CONFIG_H__
